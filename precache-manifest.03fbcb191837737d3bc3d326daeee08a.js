caa1fe536599191b1a2d196bce0bafc989df1d78
Anna Gerdii <annagerdiy@gmail.com>
2019-12-19 14:05:58 +0200

    Updates

---
+++ precache-manifest.03fbcb191837737d3bc3d326daeee08a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "591176a9c95ff5d941fc1340446e0229",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "c6b24c992f5a879cc0a1",
+    "url": "/react_dynamic-list-of-todos/static/css/main.2f44a456.chunk.css"
+  },
+  {
+    "revision": "0a660b30eeb6f888d7e2",
+    "url": "/react_dynamic-list-of-todos/static/js/2.12db86b6.chunk.js"
+  },
+  {
+    "revision": "c6b24c992f5a879cc0a1",
+    "url": "/react_dynamic-list-of-todos/static/js/main.cad3b50a.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
